80fbd7001b5dde69c1de57bf32740d16d5ef9848
ZacharyLandes <zach.g.landes@gmail.com>
2018-03-23 16:51:49 -0600

    food crud working

---
+++ db/migrations/20180323155412_create-foods-table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+    let createQuery = `CREATE TABLE foods(
+      id SERIAL PRIMARY KEY NOT NULL,
+      name TEXT,
+      calories INTEGER
+    )`
+    return knex.raw(createQuery)
+  }
+  
+  exports.down = function(knex, Promise) {
+    let dropQuery = `DROP TABLE foods`
+    return knex.raw(dropQuery)
+  }
---
+++ db/seeds/dev/foods.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  .then(function() {
+    return Promise.all([
+      knex.raw(
+        'INSERT INTO foods (name, calories) VALUES (?, ?)',
+        ["bananas",120]
+      ),
+      knex.raw(
+        'INSERT INTO foods (name, calories) VALUES (?, ?)',
+        ["tacos",220]
+      ),
+      knex.raw(
+        'INSERT INTO foods (name, calories) VALUES (?, ?)',
+        ["burritos",620]
+      )
+    ])
+  })
+}
