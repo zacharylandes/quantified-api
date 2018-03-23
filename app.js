fd7bc25249a6d1c1db23205af1bfa63260ee99f8
ZacharyLandes <zach.g.landes@gmail.com>
2018-03-23 17:25:41 -0600

    cors

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/', indexRouter);
 app.use('/api/foods', foodsRouter);
 
